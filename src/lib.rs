70911ac0b3748023c44d758b4187cb5423d5f965
stewthestew <stew156@proton.me>
2025-01-20 13:41:22 +0400

    a

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,3 @@
-// Import the calorie module
-mod calorie;
-#[allow(unused)]
-pub use calorie::*;
-#[cfg(test)]
-mod tests {
-    use crate::calorie;
-
-    #[test]
-    fn test_true() {
-        let color1 = calorie::color::foreground::truecolor(255, 255, 255);
-        let color2 = calorie::color::background::truecolor(255, 255, 255);
-        let supported = calorie::check256support();
-        println!("{color1} {color2} {supported}");
-    }
-}
+pub mod background;
+pub mod calorie;
+pub mod foreground;
